8106b53305e8fe9c7ae3091ab2cad37712a3c4c1
jensolegraulund <jensole@graulund.net>
2015-06-07 22:27:33 +0200

    fic librarys name

--- alarmrouter/start.sh
+++ alarmrouter/start.sh
@@ -2,11 +2,11 @@ cd /tmp
 
 # try to remove the repo if it already exists
 
-rm -rf mqtt; true
+rm -rf alarmrouter; true
 
 git clone https://github.com/jensoleg/MessageRouter.git
 
-cd ./mqtt
+cd ./alarmrouter
 
 npm install --unsafe-perm --production
 
